6a1599a87552c4fa86763208ee6858b793affb89
JAYASIMHAREDDYYELETI <jayayeleti44@gmail.com>
2025-09-08 22:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function findLevel(){
+	let count = 0;
+	let currentElement = level;
+	while (currentElement !== null) {
+			currentElement = currentElement.parentNode;
+		  count++
+		}
+	console.log(count,`The level of the element is:`)
+}
+findLevel();
